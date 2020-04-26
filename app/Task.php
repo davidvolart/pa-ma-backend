e38c82558c245f4b87eec2825b0775e77f6aefe9
David <dvolart@edu.tecnocampus.cat>
2020-04-26 13:02:36 +0200

    add tasks model and migration

---
+++ app/Task.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    public $timestamps = false;
+
+    protected $table = 'Tasks';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'child_id', 'name', 'description', 'date', 'user_id',
+    ];
+
+    function child()
+    {
+        return $this->belongsTo('App\Child');
+    }
+
+    function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
---
+++ database/migrations/2020_04_26_105557_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('child_id')->unsigned();
+            $table->foreign('child_id')->references('id')->on('children');
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->date('date');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
